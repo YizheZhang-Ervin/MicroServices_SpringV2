2b7de3713f33c60e022641a8265296a8178bd9c4
zhang.yizhe@northeastern.edu <zhang.yizhe@northeastern.edu>
2021-02-07 10:49:54 -0500

    add transmit data between back and front

--- README.md
+++ README.md
@@ -2,37 +2,23 @@
 Vue + Flask RESTful  
   
 # Features  
-## Frontend (static folder + index.html)  
-- Vue + ElementUI + Echarts + Axios  
-- Mouse Position  
-- GetMedia +TakePhoto  
-- Video drag + drop  
-- get latitude and longitude  
-- Echart World/China Maps  
-- Browser Parameters  
-- axios communication with backend  
-- title Clock if page Visible(2 kinds of clock)  
+## Frontend (frontend folder)  
+npm run serve  
+- Vue + ElementUI + Axios + pubsub-js    
+- 父子组件/同级组件互相传值  
+- 前后端axios传值    
+
+## Backend (app.py)  
+python -m flask run   
+- get/post的路由传参/问号传参  
   
-## Backend (FinTechAlgs + app.py)  
-cmd: python -m flask run  
-- FinTech Algorithms  
-- get动态路由传值  
-- get问号路由参数传值  
-- post传值  
+## Other  
+### Git files (.gitignore/README.md/LICENSE)  
   
-# Other  
-## ignore files (.gitignore)  
-  
-## LICENSE  
-  
-## Requirement for backend and heroku deployment (requirements.txt)  
+### Requirement for backend and heroku deployment (requirements.txt)  
 - Virtual Env libs: pip freeze > requirements.txt  
 - Dependency libs: pipreqs ./  
   
-## Heroku File (Procfile)  
-- heroku run python xxx
-  
-## Common Flask Templates (Flask_templates Folder)  
-  
-## Gold Analysis (GoldAnalysis Folder)  
+### Heroku File (Procfile)  
+- heroku run python xxx  
   
--- app.py
+++ app.py
@@ -1,13 +1,6 @@
 from flask import Flask, jsonify, render_template, request
 from flask_restful import Api,Resource,reqparse
 from flask_cors import CORS
-import click
-# import os
-# import sys
-# sys.path.append("..") 
-# from FinTechAlgs import Bonds_NSModel,Bonds_NSSModel
-# BASE_DIR = os.path.dirname(__file__)
-
 
 # Initialize Flask
 app = Flask(__name__,static_folder='static',template_folder='static',static_url_path="")
@@ -36,9 +29,6 @@ class jsonAPI(Resource):
     def get(self,key):
         try:
             jsonObj = {"result":key,'function':1}
-            # 如果要返回代码内容
-            # f1 = open(f"{BASE_DIR}/FinTechAlgs/Equity_FamaFrench.py")
-            # rst = f1.readlines()
             return jsonify(jsonObj)
         except Exception:
             return jsonify({"error":"error"})
@@ -78,13 +68,5 @@ class jsonAPI2(Resource):
             return jsonify({"error":"error"})
 api.add_resource(jsonAPI2, '/api/')
 
-# 执行方法: python app.py  --mode=True --host="127.0.0.2" --port="5000"
-@click.command()
-@click.option('--mode', default=True, type=click.Choice(["True", "False"]), help="True:develop / False:production")
-@click.option('--host', default='127.0.0.1', type=str, help='x.x.x.x')
-@click.option('--port', default='8080', type=str, help='1-65535')
-def run(host,port,mode):
-    app.run(debug=mode,host=host,port=port)
-
 if __name__ == '__main__':
-    run()
+    app.run(debug=False)
