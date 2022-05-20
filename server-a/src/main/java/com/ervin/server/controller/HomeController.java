e219fd8feb17aa44b603f88c9edbc6d8af9d437c
ErvinZhang <zhang.yizhe@northeastern.edu>
2022-05-20 16:54:20 +0800

    add conf

---
+++ server-a/src/main/java/com/ervin/server/controller/HomeController.java
@@ -0,0 +1,36 @@
+package com.ervin.server.controller;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home(@RequestParam String urls){
+        List<String> urlLis = Arrays.asList(urls.split(","));
+        System.out.println(urlLis.get(0));
+        System.out.println(urlLis.get(0).isEmpty());
+        System.out.println(CollectionUtils.isEmpty(urlLis));
+        System.out.println(ObjectUtils.isEmpty(urlLis));
+        return urlLis.get(0);
+    }
+
+    @GetMapping("/encode")
+    public static String encodePassword(@RequestParam String password) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//        String encodedPassword = encoder.encode(password);
+        String encodedPassword = "$2a$10$SnIXGO9mfuKrpP0TweZLTuuLlwR4.rwnOd0cjtpVOEZzY/f/xdcDq";
+        boolean result = encoder.matches(password,encodedPassword);
+        return encoder.encode(password)+ "," +result;
+    }
+}
